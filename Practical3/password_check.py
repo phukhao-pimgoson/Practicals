dad3d58ceec3b2c69f6a24b917e6f64a3c0ccf6e
phoenyx <ph.03.nyx.c@gmail.com>
2020-04-03 17:34:50 +0800

    refactored the file into multiple functions

--- Practical3/password_check.py
+++ Practical3/password_check.py
@@ -1,10 +1,19 @@
 def main():
+    password = get_password()
+    censor_password(password)
+
+
+def censor_password(password):
+    for a in password:
+        print("*", end="")
+
+
+def get_password():
     password = input("Enter password of 8 characters minimum: ")
     while len(password) < 8:
         print("Password must have at least 8 characters inclusive")
         password = input("Enter password of 8 characters minimum: ")
-    for a in password:
-        print("*", end="")
+    return password
 
 
 main()
