1089621a6fda5cc19d3b24a9bb6b5a623c2352fe
phoenyx <ph.03.nyx.c@gmail.com>
2020-04-25 17:06:15 +0800

    Made necessary refactoring

--- Practical5/hex_colours.py
+++ Practical5/hex_colours.py
@@ -1,11 +1,16 @@
 COLOUR_CODES = {"000000": "Black", "FFFFFF": "White", "7F7F7F": "Gray50", "FF0000": "Red1", "00FF00": "Green1",
                 "0000FF": "Blue1", "FFFF00": "Yellow1", "FF00FF": "Magenta1", "00FFFF": "Cyan1", "A020F0": "purple1"}
-print(COLOUR_CODES)
 
-user_code = (input("Enter colour code: ")).upper()
-while user_code != "":
-    if user_code in COLOUR_CODES:
-        print(user_code, "is", COLOUR_CODES[user_code])
-    else:
-        print("Code does not exist in dictionary")
-    user_code = input("Enter colour code: ")
+
+def main():
+    print(COLOUR_CODES)
+    user_code = (input("Enter colour code: ")).upper()
+    while user_code != "":
+        if user_code in COLOUR_CODES:
+            print(user_code, "is", COLOUR_CODES[user_code])
+        else:
+            print("Code does not exist in dictionary")
+        user_code = input("Enter colour code: ")
+
+
+main()
--- Practical5/word_occurrences.py
+++ Practical5/word_occurrences.py
@@ -1,11 +1,15 @@
-word_count = {}
-string = (input("Enter your text string here: ")).split()
-if len(string) == 0:
-    string.append("VOID")
-for word in string:
-    frequency = word_count.get(word, 0)
-    word_count[word] = frequency + 1
-string = list(word_count.keys())
-string.sort()
-for word in string:
-    print("{:{}}: {}".format(word, max((len(word) for word in string)), word_count[word]))
+def main():
+    word_count = {}
+    string = (input("Enter your text string here: ")).split()
+    if len(string) == 0:
+        string.append("VOID")
+    for word in string:
+        frequency = word_count.get(word, 0)
+        word_count[word] = frequency + 1
+    string = list(word_count.keys())
+    string.sort()
+    for word in string:
+        print("{:{}}: {}".format(word, max((len(word) for word in string)), word_count[word]))
+
+
+main()
