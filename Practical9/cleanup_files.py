5b121b989011634a52d414d13c869b9478d1d2c3
phoenyx <ph.03.nyx.c@gmail.com>
2020-05-26 16:51:19 +0800

    added prac8

---
+++ Practical9/cleanup_files.py
@@ -0,0 +1,33 @@
+"""
+CP1404/CP5632 Practical
+Cleanup inconsistent song lyrics file names
+"""
+import os
+
+
+def main():
+    """Cleanup inconsistent song lyrics file name"""
+    # Change to desired directory
+    os.chdir('Lyrics')
+    for directory_name, subdirectories, filenames in os.walk('.'):
+        print("Directory:", directory_name)
+        print("Contains Subdirectories:", subdirectories)
+        print("And Files:", filenames)
+        print("Current working directory is: {}".format(os.getcwd()))
+
+        for filename in filenames:
+            new_name = get_fixed_filename(filename)
+            print("Renaming {} to {}".format(filename, new_name))
+
+            full_name = os.path.join(directory_name, filename)
+            new_name = os.path.join(directory_name, new_name)
+            os.rename(full_name, new_name)
+
+
+def get_fixed_filename(filename):
+    """Return a 'fixed' version of filename."""
+    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    return new_name
+
+
+main()
